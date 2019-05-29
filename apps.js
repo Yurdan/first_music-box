a5675d264ea1f3f79a7644021b0483f322e203bb
Yurdanov Sergey <yurdik1987@gmail.com>
2019-05-29 14:30:13 +0300

    music box

---
+++ apps.js
@@ -0,0 +1,112 @@
+import db from './posts.js'
+
+db.myPlaylist = [];
+
+// function create HTML element
+const createElement = (tag) => {
+  return  document.createElement(tag);
+}
+
+// function add element in 
+const addElementTo = (element, to) => {
+    to.appendChild(element);
+    return element;
+}
+
+// function add class
+const addClass = (elem, className) => {
+    elem.classList.add(className);
+}
+// function add post im myPlaylist
+const addToMyPlaylist = (currentId) => {
+    let itemArray;
+    for(let key in db) {
+        itemArray = db[key]; // массив
+        for (var i = 0; i < itemArray.length; i++) { //перебор массива
+            let itemObj = itemArray[i]; //объект
+            if (itemObj.id == +currentId && !db.myPlaylist.find(x => x.id == +currentId))
+            return db.myPlaylist.push(itemObj);
+          }
+        }
+    }
+    
+const createPostContainer = (obj) => {
+  const postContainer = createElement('div');
+  const postAuthor = createElement('p');
+  const postDescription = createElement('p');
+  const postTitle = createElement('h3');
+  const postImg = createElement('img');
+  const postAddButton = createElement('button');
+
+  addClass(postContainer, 'post');
+  addClass(postAuthor, 'author');
+  addClass(postTitle, 'title');
+  addClass(postImg, 'img');
+  addClass(postAddButton, 'addToMyPlaylist');
+
+  postAuthor.innerText = obj.author;
+  postDescription.innerText = obj.description;
+  postTitle.innerText = obj.title;
+  postImg.setAttribute('src', obj.imgUrl);
+  postAddButton.innerText = 'To playlist';
+  postAddButton.id = obj.id;
+
+  addElementTo(postTitle, postContainer);
+  addElementTo(postImg, postContainer);
+  addElementTo(postDescription, postContainer);
+  addElementTo(postAuthor, postContainer);
+  addElementTo(postAddButton, postContainer);
+
+
+//   show only one post by click on img
+    postImg.addEventListener('click', () => {
+    mainContainer.innerHTML = '';
+    addElementTo(postContainer, mainContainer);
+    });
+
+// click on button => call function add post im myPlaylist (20) 
+    postAddButton.addEventListener('click', event => {
+        let currentId = event.currentTarget.id;
+        addToMyPlaylist(+currentId);
+});
+
+  return postContainer;
+}
+
+const renderSection = () => {
+    for(let key in db) {
+        renderSections(key);
+    }
+}
+const renderSections = (key) => {
+    db[key].forEach(function(item) {
+        const postContainer = createPostContainer(item);
+        addElementTo(postContainer, mainContainer);
+    })
+
+}
+const mainContainer = createElement('section');
+mainContainer.setAttribute('id', 'app');
+addElementTo(mainContainer, document.body);
+
+window.onload = () => {
+    renderSection();
+    const hotList = document.getElementById('hotlist');
+    hotList.addEventListener('click', () => {
+        mainContainer.innerHTML = '';
+        renderSections('hotlist');
+    })
+    const homePage = document.getElementById('homePage');
+    homePage.addEventListener('click', () => {
+        mainContainer.innerHTML = '';
+        renderSection();
+        renderSections();
+    })
+    const myList = document.getElementById('myPlaylist');
+    myList.addEventListener('click', () => {
+        mainContainer.innerHTML = '';
+        renderSections('myPlaylist');
+    })
+
+}
+
---
+++ posts.js
@@ -0,0 +1,184 @@
+var library = 
+  [
+    {
+      "id":1,
+      "imgUrl": "https://picsum.photos/id/121/300/300",
+      "title": "Bla bla title",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    
+  
+    },
+    {
+      "id":2,
+      "imgUrl": "https://picsum.photos/id/122/300/300",
+      "title": "Lorem ipsum dolor sit amet",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    },
+    {
+      "id":3,
+      "imgUrl": "https://picsum.photos/id/123/300/300",
+      "title": "Lorem ipsum dolor sit amet",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    },
+    {
+      "id":4,
+      "imgUrl": "https://picsum.photos/id/124/300/300",
+      "title": "Lorem ipsum dolor sit amet",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    },
+    {
+      "id":5,
+      "imgUrl": "https://picsum.photos/id/125/300/300",
+      "title": "Lorem ipsum dolor sit amet",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    },
+    {
+      "id":6,
+      "imgUrl": "https://picsum.photos/id/126/300/300",
+      "title": "Lorem ipsum dolor sit amet",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    },
+    {
+      "id":7,
+      "imgUrl": "https://picsum.photos/id/127/300/300",
+      "title": "Lorem ipsum dolor sit amet",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    },
+    {
+      "id":8,
+      "imgUrl": "https://picsum.photos/id/128/300/300",
+      "title": "Lorem ipsum dolor sit amet",
+      "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+      "author": "John Doe"
+    }
+  ];
+
+var hotlist =
+[
+  {
+    "id":9,
+    "imgUrl": "https://picsum.photos/id/21/300/300",
+    "title": "Ipsum dolor lorem sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":10,
+    "imgUrl": "https://picsum.photos/id/22/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":11,
+    "imgUrl": "https://picsum.photos/id/23/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":12,
+    "imgUrl": "https://picsum.photos/id/25/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":13,
+    "imgUrl": "https://picsum.photos/id/26/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":14,
+    "imgUrl": "https://picsum.photos/id/27/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":15,
+    "imgUrl": "https://picsum.photos/id/28/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":16,
+    "imgUrl": "https://picsum.photos/id/29/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  }
+];
+
+var playlist =
+[
+  {
+    "id":17,
+    "imgUrl": "https://picsum.photos/id/222/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":18,
+    "imgUrl": "https://picsum.photos/id/221/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":19,
+    "imgUrl": "https://picsum.photos/id/223/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":20,
+    "imgUrl": "https://picsum.photos/id/293/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":21,
+    "imgUrl": "https://picsum.photos/id/244/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":22,
+    "imgUrl": "https://picsum.photos/id/294/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":23,
+    "imgUrl": "https://picsum.photos/id/227/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  },
+  {
+    "id":24,
+    "imgUrl": "https://picsum.photos/id/228/300/300",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Eros in cursus turpis massa tincidunt. At volutpat diam ut venenatis tellus in metus vulputate.",
+    "author": "John Doe"
+  }
+
+
+  ];
+export default {library,hotlist,playlist}; 
